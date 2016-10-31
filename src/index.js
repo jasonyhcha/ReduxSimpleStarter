6215795e8b0ebde2d850e236575f32d054539840
Jason Cha <jasonyhcha@gmail.com>
2016-10-31 03:01:35 -0700

    Finished section 1, lesson 20.

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
 
-import App from './components/app';
-import reducers from './reducers';
+import SearchBar from './components/search_bar';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const API_KEY = "AIzaSyCqr5uV8E4qE50e-7EhC5YbgANvj_c-hpM";
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+
+const App = () => {
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
+}
+
+
+ReactDOM.render(<App />, document.querySelector('.container'));
